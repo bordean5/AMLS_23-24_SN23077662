6f452b362610a3e88de81cd42ca4cfeab31bda49
Yaoming Zhang <koyisaa@outlook.com>
2023-12-21 19:45:26 +0000

    upload B1 with random forest and B2 with CNN

---
+++ B/B1.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+
+global file_path
+file_path="./Datasets/pathmnist.npz"
+
+def load_data(file_path:str):
+  """
+    load the data with file path and extract subsets
+  Args:
+        file_path:str
+  Returns:
+        train_images, val_images, test_images, train_labels, val_labels, test_labels
+  """
+  data = np.load(file_path)
+
+  train_images=data["train_images"]
+  val_images=data["val_images"]
+  test_images=data["test_images"]
+  train_labels=data["train_labels"].ravel()  #flatten the labels
+  val_labels=data["val_labels"].ravel()
+  test_labels=data["test_labels"].ravel()
+
+  data.close()
+
+  return train_images, val_images, test_images, train_labels, val_labels, test_labels
+
+def pre_processing(train_images, val_images, test_images):
+  """
+    preprocess the data with standardscaler, pca and normalization
+  Args:
+        all data images subsets
+  Returns:
+        train_images, val_images, test_images after preprocessing
+  """
+  #flatten image features
+  train_flatten_images = train_images.reshape(train_images.shape[0], -1)
+  val_flatten_images = val_images.reshape(val_images.shape[0], -1)
+  test_flatten_images = test_images.reshape(test_images.shape[0], -1)
+  # scale the input features of the data
+  scaler = StandardScaler()
+  train_standard_images = scaler.fit_transform(train_flatten_images) #only fit the train data
+  val_standard_images = scaler.transform(val_flatten_images)  
+  test_standard_images = scaler.transform(test_flatten_images)
+  # creating the PCA model
+  pca = PCA(n_components=200)
+
+  # using the PCA model on our standardized data
+  pca.fit(train_standard_images)
+
+  train_pca=pca.transform(train_standard_images)
+  val_pca=pca.transform(val_standard_images)
+  test_pca=pca.transform(test_standard_images)
+  # contains the proportion of the total variance in the data explained by each principal component
+
+  #normalize
+  min_max_scaler = preprocessing.MinMaxScaler()  #normalize
+  min_max_scaler.fit(train_pca)
+  train = min_max_scaler.transform(train_pca)
+  val = min_max_scaler.transform(val_pca)
+  test = min_max_scaler.transform(test_pca)
+  
+  return train, val, test
+
+def randomf_training(train, train_labels):
+  print("training random forest model for pathmnist...")
+  rf = RandomForestClassifier(n_estimators=250,n_jobs=4,class_weight="balanced")
+  rf =rf.fit(train, train_labels)
+
+  return rf
+
+def testing(model, test, test_labels):
+  """
+    give the report of the testing
+  Args:
+        the model and test data
+  Returns:
+       print the report
+  """
+  pred = model.predict(test)
+  conf_matrix = confusion_matrix(test_labels, pred)
+  precision = precision_score(test_labels, pred,average="weighted")
+  recall = recall_score(test_labels, pred,average="weighted")
+  f1 = f1_score(test_labels, pred,average="weighted")
+
+  print("condusion matrix:\n", conf_matrix)
+  print("Precision:", precision)
+  print("Recall:", recall)
+  print("F1 Score:", f1)
+  print("Test Accuracy:", accuracy_score(test_labels, pred))
+  print(classification_report(test_labels, pred))
+
+def demonstration():
+  train_images, val_images, test_images, train_labels, val_labels, test_labels=load_data(file_path)
+  train, val, test= pre_processing(train_images, val_images, test_images)
+  rf_model=randomf_training(train, train_labels)
+  print("\n"+"The test for the randomforest model: ")
+  testing(rf_model, test, test_labels)
+
+
+
+if __name__ == "__main__":
+  demonstration()
---
+++ B/B2.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import keras
+
+from sklearn.metrics import classification_report
+from sklearn.utils.class_weight import compute_class_weight
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.callbacks import EarlyStopping
+from keras import layers
+from keras.regularizers import l2
+
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
+keras.utils.set_random_seed(42)
+
+global file_path
+file_path="./Datasets/pathmnist.npz"
+
+def load_data(file_path:str):
+  """
+    load the data with file path and extract subsets
+  Args:
+        file_path:str
+  Returns:
+        train_images, val_images, test_images, train_labels, val_labels, test_labels
+  """
+  data = np.load(file_path)
+
+  train_images=data["train_images"]
+  val_images=data["val_images"]
+  test_images=data["test_images"]
+  train_labels=data["train_labels"].ravel()  #flatten the labels
+  val_labels=data["val_labels"].ravel()
+  test_labels=data["test_labels"].ravel()
+
+  data.close()
+  return train_images, val_images, test_images, train_labels, val_labels, test_labels
+
+def normalization(train_images, val_images,test_images):
+  train=train_images/255.0
+  val=val_images/255.0
+  test=test_images/255.0
+  return train, val ,test
+
+def model_training(train,train_labels,val,val_labels):
+  class_weights = compute_class_weight(class_weight='balanced', 
+                                     classes=np.unique(train_labels), 
+                                     y=train_labels.flatten())
+
+  class_weights_dict = {i : class_weights[i] for i in range(len(class_weights))}
+
+  model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', padding="same",input_shape=(28, 28, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu',padding="same"),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu',padding="same",kernel_regularizer=l2(0.001)),
+    layers.GlobalAveragePooling2D(),
+
+    Flatten(),
+    Dense(64, activation='relu'),
+    layers.Dropout(0.2),
+    Dense(9, activation='softmax')
+  ])
+
+  model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+  early_stopping = EarlyStopping(monitor='val_loss', patience=5, 
+                               verbose=1, mode='min', restore_best_weights=True)
+
+  history = model.fit(train, train_labels,epochs=60, class_weight=class_weights_dict,
+                    validation_data=(val, val_labels),callbacks=[early_stopping])
+
+  model.summary()
+
+  return history, model
+
+def testing(model, test, test_labels):
+  test_loss, test_acc = model.evaluate(test,  test_labels, verbose=1)
+  predictions = model.predict(test)
+  predicted_labels = np.argmax(predictions, axis=1)
+  print(classification_report(test_labels,predicted_labels)) 
+
+def demonstration():
+  train_images, val_images, test_images, train_labels, val_labels, test_labels=load_data(file_path)
+  train, val ,test=normalization(train_images,val_images,test_images)
+  history,model=model_training(train,train_labels,val,val_labels)
+  testing(model,test, test_labels)
+
+if __name__ == "__main__":
+  demonstration() 
+
